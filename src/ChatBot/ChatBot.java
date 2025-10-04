bfb734e0917d885626a70cbec68c22ebc933d2db
Кудиненко Кирило <k.o.kudynenko@student.khai.edu>
2025-10-04 15:02:16 +0300

    ChatBot 1st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,12 @@
+package ChatBot;
+
+
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Hello! My name is MrBot.\n" +
+                "I was created in 2025.");
+    }
+}
