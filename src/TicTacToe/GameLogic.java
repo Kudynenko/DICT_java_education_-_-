748e57749b4103bf11fb59cf4c8eb76d53f24a8c
Кудиненко Кирило <k.o.kudynenko@student.khai.edu>
2025-11-27 17:14:52 +0200

    TicTacToe 4-th stage

--- src/TicTacToe/GameLogic.java
+++ src/TicTacToe/GameLogic.java
@@ -5,14 +5,38 @@ import java.util.Scanner;
 public class GameLogic {
     private final Scanner scanner = new Scanner(System.in);
 
-    public void stage3() {
+    public void stage4() {
         System.out.println("Enter cells:");
-        String cells = scanner.nextLine();
-        char[] arr = cells.toCharArray();
+        char[] arr = scanner.nextLine().toCharArray();
         printBoard(arr);
 
-        String result = analyzeState(arr);
-        System.out.println(result);
+        while (true) {
+            System.out.println("Enter the coordinates:");
+            String[] input = scanner.nextLine().split(" ");
+            if (input.length != 2 || !input[0].matches("\\d+") || !input[1].matches("\\d+")) {
+                System.out.println("You should enter numbers!");
+                continue;
+            }
+
+            int x = Integer.parseInt(input[0]);
+            int y = Integer.parseInt(input[1]);
+
+            if (x < 1 || x > 3 || y < 1 || y > 3) {
+                System.out.println("Coordinates should be from 1 to 3!");
+                continue;
+            }
+
+            int index = (x - 1) * 3 + (y - 1);
+            if (arr[index] != '_') {
+                System.out.println("This cell is occupied! Choose another one!");
+                continue;
+            }
+
+            arr[index] = 'X';
+            break;
+        }
+
+        printBoard(arr);
     }
 
     private void printBoard(char[] cells) {
@@ -21,38 +45,5 @@ public class GameLogic {
             System.out.println("| " + cells[i] + " " + cells[i+1] + " " + cells[i+2] + " |");
         }
         System.out.println("---------");
-    }
-
-    private String analyzeState(char[] arr) {
-        boolean xWins = checkWinner(arr, 'X');
-        boolean oWins = checkWinner(arr, 'O');
-        int countX = countSymbol(arr, 'X');
-        int countO = countSymbol(arr, 'O');
-
-        if (xWins && oWins || Math.abs(countX - countO) > 1) return "Impossible";
-        if (xWins) return "X wins";
-        if (oWins) return "O wins";
-        if (new String(arr).contains("_")) return "Game not finished";
-        return "Draw";
-    }
-
-    private boolean checkWinner(char[] arr, char symbol) {
-        int[][] lines = {
-                {0,1,2},{3,4,5},{6,7,8},
-                {0,3,6},{1,4,7},{2,5,8},
-                {0,4,8},{2,4,6}
-        };
-        for (int[] line : lines) {
-            if (arr[line[0]] == symbol && arr[line[1]] == symbol && arr[line[2]] == symbol) return true;
-        }
-        return false;
-    }
-
-    private int countSymbol(char[] arr, char symbol) {
-        int count = 0;
-        for (char c : arr) {
-            if (c == symbol) count++;
-        }
-        return count;
     }
 }
--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -4,7 +4,7 @@ public class TicTacToe {
     public static void main(String[] args) {
         GameLogic game = new GameLogic();
 
-        game.stage3();
+        game.stage4();
 
     }
 }
