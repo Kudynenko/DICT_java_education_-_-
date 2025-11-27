1652c887a3b1b4011b9f7ce9552478dd9afb53d1
Кудиненко Кирило <k.o.kudynenko@student.khai.edu>
2025-11-27 16:53:34 +0200

    TicTacToe 2-nd stage

--- src/TicTacToe/GameLogic.java
+++ src/TicTacToe/GameLogic.java
@@ -1,11 +1,21 @@
 package TicTacToe;
 
+import java.util.Scanner;
 
 public class GameLogic {
+    private final Scanner scanner = new Scanner(System.in);
 
-    public void stage1() {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+    public void stage2() {
+        System.out.println("Enter cells:");
+        String cells = scanner.nextLine();
+        printBoard(cells.toCharArray());
+    }
+
+    private void printBoard(char[] cells) {
+        System.out.println("---------");
+        for (int i = 0; i < 9; i += 3) {
+            System.out.println("| " + cells[i] + " " + cells[i+1] + " " + cells[i+2] + " |");
+        }
+        System.out.println("---------");
     }
 }
